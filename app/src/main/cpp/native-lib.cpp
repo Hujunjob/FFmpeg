a57c58e808a3882f9f9e0a2124f4d436163f225a
JUN HU <huj@hiscene.com>
2020-01-07 21:26:10 +0800

    Add : 添加FFmpeg库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
 
+extern "C"{
+#include "libavcodec/avcodec.h"
+#include <libavutil/avutil.h>
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_hujun_ffmpeg_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(av_version_info());
 }
